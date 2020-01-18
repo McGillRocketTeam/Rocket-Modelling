e7122fcfbf2dd830434be15f074bad1ae31e2eeb
junhha-yoo <junha.yoo@mail.mcgill.ca>
2020-01-18 16:21:14 -0500

    Update nozzle Some name changes follow as: flow_area_relations -> nozzle_area_ratio pressure_outlet -> get_exit_pressure

--- Nozzle.py
+++ Nozzle.py
@@ -18,24 +18,24 @@ class Nozzle:
         self.outlet_area = b
         self.mass = c
 
-    def flow_area_realtion(self, a, a_star, gamma):
+    def nozzle_area_ratio(self, a, a_star, gamma):
         m = Symbol('M')
         sol = solve((a / a_star) - (1 / m) * ((2 / (gamma + 1)) * (1 + (gamma - 1) / 2) * m ** 2) ** ((gamma + 1) / (2 * (gamma - 1))), m)
-        print("Flow area realton:",sol)
+        print("Nozzle area ratio:",sol)
         return sol
     
-    def mass_flow_rate(self, pressure, temperature,throat_area, mech_num = 1,
+    def mass_flow_rate(self, pressure, temperature,throat_area, mach_num = 1,
                        gamma = 1.4, R_prime = 8.314, M_prime = 30):
         m = Symbol('m')
         sol = solve(m - throat_area * (pressure/temperature) * (gamma/(R_prime/M_prime))**(0.5)
-        * (mech_num/(1+((gamma-1)*mech_num**2)/2) ** ((gamma+1)/(2*(gamma-1)))))
+        * (mach_num/(1+((gamma-1)*mach_num**2)/2) ** ((gamma+1)/(2*(gamma-1)))))
         print("Mass flow rate:",sol)
         return sol
     
-    def pressure_condition(p0,mech_num, gamma = 1.4):
+    def get_exit_pressure(p0,mach_num, gamma = 1.4):
         p = Symbol('p')
-        sol = solve(p0/p - ((1+(gamma-1)* mech_num**2)/2) ** (gamma/(gamma-1)),m)
-        print("Pressure:",sol)
+        sol = solve(p0/p - ((1+(gamma-1)* mach_num**2)/2) ** (gamma/(gamma-1)),m)
+        print("Exit Pressure:",sol)
         return sol
 
     def get_thrust_from_nozzle(self, m_dot, r_cnst, temp_not, p_e, p_o, p_amb, a_e, gamma=1.4):
