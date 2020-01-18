57425d65c179d98c13886c580ed03b580efb73b4
Alex Stedman <alexander.stedman@mail.mcgill.ca>
2020-01-18 14:30:52 -0500

    Update nozzle class and requirements

--- Nozzle.py
+++ Nozzle.py
@@ -4,6 +4,8 @@ Created on Fri Jan 17 16:20:40 2020
 
 @author: sunge
 """
+from sympy import Symbol
+from sympy.solvers import solve
 
 class Nozzel:
     throat_area = 1
@@ -13,3 +15,8 @@ class Nozzel:
         self.throat_area = a
         self.outlet_area = b
         self.mass = c
+
+    def flow_area_realtion(self, a, a_star, gamma):
+        m = Symbol('M')
+        sol = solve((a / a_star) - (1 / m) * ((2 / (gamma + 1)) * (1 + (gamma - 1) / 2) * m ** 2) ** ((gamma + 1) / (2 * (gamma - 1))), m)
+        print(sol)
