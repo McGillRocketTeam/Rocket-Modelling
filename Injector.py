314929364610d8ec438eda4918fee799a6a60ebd
Moob6622 <moob6622@gmail.com>
2020-01-21 15:35:54 -0500

    Injector : Change inputs for design and converge methods to only ask for Temp and density of tank and combustion chamber, I derive every other property via CoolProp. I chose Temp and density as they are listed as the fastest inputs in the CoolProp documentation

--- Injector.py
+++ Injector.py
@@ -5,7 +5,7 @@ Created on Fri Jan 17 16:13:36 2020
 @author: sunge
 """
 from math import sqrt
-import CoolProp as cp
+from CoolProp.CoolProp import PropSI
 
 class Injector:
     area = 1
@@ -16,11 +16,18 @@ class Injector:
     temp = 1
 
     def __init__ (self,A,L,M):
-        self.area = A_o
+        self.area = A
         self.length = L
         self.mass = M
 
-    def design (self, P_tank, rho_tank, h_tank,  P_cc, rho_cc, h_cc, P_vap, C_d, m_dot):
+    def design (self,T_tank, rho_tank,  T_cc, rho_cc, P_vap, C_d, m_dot):
+        # Find missing properties from CoolProp
+        P_tank = PropSI('P', 'T', T_tank, 'D', rho_tank, 'N2O' )
+        h_tank = PropSI('H', 'T', T_tank, 'D', rho_tank, 'N2O' )
+
+        P_cc =  PropSI('P', 'T', T_cc, 'D', rho_cc, 'N2O' ) 
+        h_cc =  PropSI('H', 'T', T_cc, 'D', rho_cc, 'N2O' )
+
         #break up parts of the equations for readability
         d_P = P_tank-P_cc
         d_h = h_tank-h_cc
@@ -30,11 +37,18 @@ class Injector:
         m_inc_coef = (kappa/(1+kappa))*sqrt(2*rho_tank*d_P)
         m_hem_coef = (1/(1+kappa))*sqrt(2*rho_cc*d_h)
 
-        area = sqrt(m_dot/((C_d**2)*(m_inc_coef + m_hem_coef))
+        area = sqrt(m_dot/((C_d**2)*(m_inc_coef + m_hem_coef)))
         return area
 
 
-    def converge (self, P_tank, rho_tank, h_tank,  P_cc, rho_cc, h_cc,  P_vap, C_d):
+    def converge (self, T_tank, rho_tank,  T_cc, rho_cc,  P_vap, C_d):
+        # Find missing properties from CoolProp
+        P_tank = PropSI('P', 'T', T_tank, 'D', rho_tank, 'N2O' )
+        h_tank = PropSI('H', 'T', T_tank, 'D', rho_tank, 'N2O' )
+
+        P_cc =  PropSI('P', 'T', T_cc, 'D', rho_cc, 'N2O' ) 
+        h_cc =  PropSI('H', 'T', T_cc, 'D', rho_cc, 'N2O' )
+
         #break up parts of the equations for readability
         d_P = P_tank - P_cc
         d_h = h_tank - h_cc
